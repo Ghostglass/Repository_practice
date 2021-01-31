dbec6f7a3723da8267b3de20af7f408d1d68a6b3
Kim <64843479+Ghostglass@users.noreply.github.com>
2021-01-31 11:48:26 +0000

    Added new HTML, CSS, Javascript, inc.
    Some HTML thrown in the mix here.
    Created a 'Moon Lagoon' HTML DOC 
    Happy coders!

---
+++ script.js
@@ -0,0 +1,21 @@
+const list = document.createElement('ul');
+const info = document.createElement('p');
+const html = document.querySelector('html');
+
+info.textContent = 'Below is a dynamic list. Click anywhere outside the list to add a new list item. Click an existing list item to change its text to something else.';
+
+document.body.appendChild('spaceLocations');
+document.body.appendChild(earthMoon);
+
+html.onclick = function() {
+  const listItem = document.createElement('li');
+  const listContent = prompt('Space Locations');
+  listItem.textContent = MoonLagoon;
+  list.appendChild();
+
+  listItem.onclick = function(e) {
+    e.stopPropagation();
+    const listContent = prompt('Enter new content for your list item');
+    this.textContent = listContent;
+  }
+}
